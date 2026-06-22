d31492c0a99fc256a72427beee99286f9c015930
ByKurebo <kmlresearchlabs@gmail.com>
2026-06-22 19:28:26 -0300

    Update on cowsay pkg: tryed to implement line break on long phrases

--- BIOS/src/pkgs/cowsay.py
+++ BIOS/src/pkgs/cowsay.py
@@ -1,16 +1,22 @@
+from textwrap import wrap
+
 def main_call(COMMANDS, MANIFEST=None):
     COMMANDS["cowsay"] = cowsay
 
-def cowsay(cmd):
-    phrase = " ".join(cmd[1:])
-    leny = len(phrase)
 
-    print(rf""" /{"=" * leny}\
- |{phrase}|
- \{"=" * leny}/
-             \  ^__^
+def cowsay(cmd, width=40):
+    phrase = " ".join(cmd[1:])
+    lines = wrap(phrase, width=width) or [""]
+
+    max_len = max(len(line) for line in lines)
+
+    print(f" /{'=' * max_len}\\")
+    for line in lines:
+        print(f" |{line.ljust(max_len)}|")
+    print(f" \\{'=' * max_len}/")
+    print(r"""             \  ^__^
               \ (oo)\_______
-               \(__)\       )\/\
+               (__)\       )\/\
                     ||----w |
                     ||     ||
 """)
